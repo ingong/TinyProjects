ee15613fdcdc559626b6715c87e2f1a098d174e8
leeinsong <insong1216@gmail.com>
2021-05-28 15:47:37 +0900

    [add] 기본으로 컴포넌트 작성

--- lotto/src/App.js
+++ lotto/src/App.js
@@ -1,5 +1,65 @@
-function App() {
-  return <div className='App'></div>;
-}
+import React, { useState, useEffect, useMemo } from "react";
+import Info from "./Info";
+
+const App = () => {
+  const [color, setColor] = useState("");
+  const [season, setSeason] = useState("");
+
+  const onChangeHandler = (e) => {
+    if (e.target.id === "color") setColor(e.target.value);
+    else setSeason(e.target.value);
+  };
+
+  return (
+    <div className='App'>
+      <div>
+        <label>
+          What is your favorite color of rainbow ?
+          <input id='color' value={color} onChange={onChangeHandler} />
+        </label>
+      </div>
+      <div>
+        What is your favorite Season ?
+        <label>
+          <input
+            type='radio'
+            name='season'
+            value='Spring'
+            onChange={onChangeHandler}
+          />
+          Spring
+        </label>
+        <label>
+          <input
+            type='radio'
+            name='season'
+            value='Summer'
+            onChange={onChangeHandler}
+          />
+          Summer
+        </label>
+        <label>
+          <input
+            type='radio'
+            name='season'
+            value='Fall'
+            onChange={onChangeHandler}
+          />
+          Fall
+        </label>
+        <label>
+          <input
+            type='radio'
+            name='season'
+            value='Winter'
+            onChange={onChangeHandler}
+          />
+          Winter
+        </label>
+      </div>
+      <Info color={color} season={season} />
+    </div>
+  );
+};
 
 export default App;
---
+++ lotto/src/Info.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+const getColorKor = (color) => {
+  console.log("getColorKor");
+  switch (color) {
+    case "red":
+      return "빨강";
+    case "orange":
+      return "주황";
+    case "yellow":
+      return "노랑";
+    case "green":
+      return "초록";
+    case "blue":
+      return "파랑";
+    case "navy":
+      return "남";
+    case "purple":
+      return "보라";
+    default:
+      return "레인보우";
+  }
+};
+
+const getSeason = (season) => {
+  console.log("getSeason");
+  switch (season) {
+    case "Spring":
+      return "봄";
+    case "Summer":
+      return "여름";
+    case "Fall":
+      return "가을";
+    case "Winter":
+      return "겨울";
+    default:
+      return "아직 잘 모름";
+  }
+};
+
+const Info = ({ color, season }) => {
+  const colorKor = getColorKor(color);
+  const seasonKor = getSeason(season);
+
+  return (
+    <div className='info-wrapper'>
+      <br></br>
+      <span>
+        제가 가장 좋아하는 색은 <b>{colorKor}</b> 이고,{" "}
+      </span>
+      <br></br>
+      <span>
+        좋아하는 계절은 <b>{seasonKor}</b> 입니다.
+      </span>
+    </div>
+  );
+};
+
+export default Info;
